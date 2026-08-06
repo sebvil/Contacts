909a0bf67acac6746176942f6b52c33952af5896
sebvil <github.somatotropically@simplelogin.co>
2026-08-05 21:23:15 -0500

    Add ContactListItem component and basic preview functinality

---
+++ app/shared/src/commonMain/kotlin/com/sebastianvm/contacts/designsys/components/PreviewUtils.kt
@@ -0,0 +1,24 @@
+package com.sebastianvm.contacts.designsys.components
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.PreviewFontScale
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.tooling.preview.PreviewScreenSizes
+import com.sebastianvm.contacts.designsys.theme.AppTheme
+import com.sebastianvm.contacts.designsys.theme.Dimensions
+
+@PreviewLightDark @PreviewFontScale public annotation class PreviewComponent
+
+@PreviewLightDark @PreviewFontScale @PreviewScreenSizes public annotation class PreviewScreens
+
+@Composable
+public fun ComponentPreview(modifier: Modifier = Modifier, content: @Composable (() -> Unit)) {
+    AppTheme {
+        Surface(modifier = modifier.padding(all = Dimensions.ScreenEdgePadding)) {
+            content()
+        }
+    }
+}
