055fa7114c12d25929ad28baeb33f68f9b2799d1
Sebas <github.somatotropically@simplelogin.co>
2026-08-05 21:24:31 -0500

    Add ContactListItem component and basic preview functinality (#10)

---
+++ app/shared/src/commonMain/kotlin/com/sebastianvm/contacts/designsys/components/ContactListItem.kt
@@ -0,0 +1,32 @@
+package com.sebastianvm.contacts.designsys.components
+
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.sebastianvm.contacts.designsys.components.ContactListItem.State
+import kotlin.uuid.Uuid
+
+internal object ContactListItem {
+
+    data class State(val id: Uuid, val name: String)
+
+    @Composable
+    operator fun invoke(state: State, modifier: Modifier = Modifier) {
+        ListItem(
+            headlineContent = {
+                Text(text = state.name)
+            },
+            modifier = modifier,
+        )
+    }
+}
+
+internal class PreviewsContainer {
+
+    @PreviewComponent
+    @Composable
+    private fun Preview() {
+        ComponentPreview { ContactListItem(State(Uuid.random(), "Elliot")) }
+    }
+}
---
+++ app/shared/src/commonMain/kotlin/com/sebastianvm/contacts/designsys/theme/AppTheme.kt
@@ -0,0 +1,16 @@
+package com.sebastianvm.contacts.designsys.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+
+@Composable
+public fun AppTheme(content: @Composable () -> Unit) {
+    MaterialTheme(
+        colorScheme = if (isSystemInDarkTheme()) darkColorScheme() else lightColorScheme()
+    ) {
+        content()
+    }
+}
